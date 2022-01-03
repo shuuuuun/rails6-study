3c68c1050e9eaeb65f2bd3b9fd594f4d88158ce3
shuuuuuny <shuuuuuny+git@gmail.com>
2022-01-03 15:17:46 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#top'
 
-  # get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'sessions#create'
   post 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 end
